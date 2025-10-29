e2027b162bada56dbe42892b6edab512db0b114e
keyserjose044 <josemarquez044@gmail.com>
2025-10-29 15:48:44 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// No adapter needed for GitHub Pages (static output)
 export default defineConfig({
-  site: "https://lifeloggerz.com",
-  base: "/",                 // root domain
-  output: "static",          // default, but explicit is fine
-  integrations: [mdx(), sitemap()],
+  site: 'https://www.jomaconcrete.com',
+  integrations: [sitemap()],
+  output: 'static',
+  trailingSlash: 'never',
 });
